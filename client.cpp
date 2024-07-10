92e5413a0c8a1e7c38bf88b7bc466e4d225891c1
Camille Assie <cassie@z2r1p3.42lyon.fr>
2024-07-10 12:36:22 +0200

    tutu

---
+++ client.cpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cassie <cassie@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/10 09:59:39 by cassie            #+#    #+#             */
+/*   Updated: 2024/07/10 10:00:54 by cassie           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Client side C program to demonstrate Socket
+// programming
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+
+int main(int argc, char const* argv[])
+{
+	int status, valread, client_fd;
+	struct sockaddr_in serv_addr;
+	std::string hello = "Hello from client";
+	char buffer[1024] = { 0 };
+	if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("\n Socket creation error \n");
+		return -1;
+	}
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+
+	// Convert IPv4 and IPv6 addresses from text to binary
+	// form
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
+		<= 0) {
+		printf(
+			"\nInvalid address/ Address not supported \n");
+		return -1;
+	}
+
+	if ((status
+		= connect(client_fd, (struct sockaddr*)&serv_addr,
+				sizeof(serv_addr)))
+		< 0) {
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+	send(client_fd, hello.c_str(), hello.size(), 0);
+	printf("Hello message sent\n");
+	valread = read(client_fd, buffer,
+				1024 - 1); // subtract 1 for the null
+							// terminator at the end
+	printf("%s\n", buffer);
+
+	// closing the connected socket
+	close(client_fd);
+	return 0;
+}
