11a0b1d6ef07bf70f69f10309d223d5037c3a258
Romain Jacq <rjacq@z1r1p4.42lyon.fr>
2024-07-18 11:28:14 +0200

    add CLient main

---
+++ src/Client.cpp
@@ -0,0 +1,46 @@
+#include "Client.hpp"
+
+Client::Client(std::string nickname, std::string username, std::string realname, std::string hostname, std::string server, const int &socket):
+	_nickname(nickname), _username(username), _realname(realname), _hostname(hostname), _server(server), _socket(socket)
+{
+	_isOP = false;
+}
+
+Client::~Client()
+{}
+
+void	Client::SetNickname(std::string nickname)
+{
+	_nickname = nickname;
+}
+
+void	Client::SetUsername(std::string username)
+{
+	_username = username;
+}
+
+void	Client::SetIsOP()
+{
+	_isOP = true;
+	_nickname += '@';
+}
+
+std::string	Client::GetNickname() const
+{
+	return _nickname;
+}
+
+std::string	Client::GetUsername() const
+{
+	return _username;
+}
+
+std::string	Client::GetRealname() const
+{
+	return _realname;
+}
+
+bool	Client::GetIsOP() const
+{
+	return _isOP;
+}
---
+++ src/Client.hpp
@@ -0,0 +1,30 @@
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+
+#include <string>
+
+class Client
+{
+	public:
+		Client(std::string nickname, std::string username, std::string realname, std::string hostname, std::string server, const int &socket);
+		~Client();
+
+		void		SetNickname(std::string nickname);
+		void		SetUsername(std::string username);
+		void		SetIsOP();
+		std::string	GetNickname() const;
+		std::string	GetUsername() const;
+		std::string	GetRealname() const;
+		bool		GetIsOP() const;
+
+	private:
+		std::string _nickname; // 9 char and unique ; name which appears for other users
+		std::string _realname; // name of the host ; just additional information about you
+		std::string _username; // username on the host ; name you use to login, allows system to identify you
+		std::string _hostname;
+		const std::string _server;   // server to which it's connected
+		int			_socket;
+		bool		_isOP;
+};
+
+#endif
