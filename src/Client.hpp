1173f67cc9d5b0c1865273be926ab277e5e3cf16
Romain Jacq <rjacq@z4r4p2.42lyon.fr>
2024-07-19 11:37:21 +0200

    corrected Client

--- src/Client.hpp
+++ src/Client.hpp
@@ -15,14 +15,16 @@ class Client
 		std::string	GetNickname() const;
 		std::string	GetUsername() const;
 		std::string	GetRealname() const;
+		int			GetSocket() const;
+		void		SetSocket(int i);
 		bool		GetIsOP() const;
 
 	private:
 		std::string _nickname; // 9 char and unique ; name which appears for other users
-		std::string _realname; // name of the host ; just additional information about you
 		std::string _username; // username on the host ; name you use to login, allows system to identify you
+		std::string _realname; // name of the host ; just additional information about you
 		std::string _hostname;
-		const std::string _server;   // server to which it's connected
+		std::string _server;   // server to which it's connected
 		int			_socket;
 		bool		_isOP;
 };
