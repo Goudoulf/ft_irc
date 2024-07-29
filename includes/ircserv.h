5dd70147b7a496ea1abf4b2d6b131da7730ff419
Romain Jacq <rjacq@z3r12p1.42lyon.fr>
2024-07-29 13:27:41 +0200

    add includes from main

---
+++ includes/ircserv.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ircserv.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cassie <cassie@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/28 07:55:03 by cassie            #+#    #+#             */
+/*   Updated: 2024/07/28 07:55:42 by cassie           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IRCSERV_H
+#define IRCSERV_H
+
+#include <cerrno>
+#include <cstring>
+#include <iostream>
+#include <netinet/in.h>
+#include <string>
+#include <strings.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <fcntl.h>
+
+#endif
