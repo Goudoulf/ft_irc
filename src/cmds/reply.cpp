2e9098284e837a591da20eeafa44afa394916a3e
Camille Assie <cassie@z3r7p1.42lyon.fr>
2024-08-21 08:25:38 +0200

    feat:Add reply server command function

---
+++ src/cmds/reply.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reply.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cassie <cassie@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/21 07:56:09 by cassie            #+#    #+#             */
+/*   Updated: 2024/08/21 07:57:21 by cassie           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ircserv.h"
+#include "../Client.hpp"
+
+void	reply_server(std::string command,  Client &client, std::string message)
+{
+	int sd = client.GetSocket();
+	std::string nickname = client.GetNickname();
+	std::string rpl(":127.0.0.1 " + command + " " + nickname + " " + message +"\r\n");
+	std::cout << "Reply = " << rpl << std::endl;
+	send(sd, rpl.c_str(), rpl.length(), 0);
+}
+
+void	message_server(std::string prefix, std::string command, std::string target, std::string message)
+{
+
+}
