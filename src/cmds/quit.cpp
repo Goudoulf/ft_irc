980e8a2a6e6c4ebd5182a825f0b28826762ad313
Camille Assie <cassie@z3r7p1.42lyon.fr>
2024-08-21 16:11:06 +0200

    feat: add quit command not fully working

--- src/cmds/quit.cpp
+++ src/cmds/quit.cpp
@@ -1,8 +1,15 @@
 #include "../../includes/cmds.h"
+#include <algorithm>
 
 void	quit(Client &client, IRCServer &server)
 {
 	std::cout << "_____quit_____" << std::endl;
+	std::string buf = client.GetBufferString();
+	std::string param (buf.substr(buf.find(":"), buf.find_first_of(" \r\n\0", 5) - 5));
+	std::cout << "PARM =" << param << std::endl;
+	for (std::vector<Client*>::iterator _it = server.getClients()->begin(); _it != server.getClients()->end(); _it++) {
+			message_server((*_it)->GetNickname(), "QUIT", client, param);
+	}
 	(void)server;
 	(void)client;
 }
