4ed61269e5b42e82ec4e026605da7f19d0547c92
Camille Assie <cassie@z3r7p1.42lyon.fr>
2024-08-26 11:17:03 +0200

    feat: Add log time

--- src/debug/debug.cpp
+++ src/debug/debug.cpp
@@ -15,8 +15,19 @@
 #include <string>
 #include "../../includes/color.h"
 #include "../../includes/debug.h"
+#define MAX_SIZE 80
 
 LogLevel currentLogLevel = DEBUG;
+
+std::string log_time()
+{
+	time_t timestamp = time( NULL );
+    struct tm * pTime = localtime( & timestamp );
+
+    char buffer[ MAX_SIZE ];
+    strftime( buffer, MAX_SIZE, "%d/%m/%Y %H:%M:%S", pTime );
+	return (buffer);
+}
 
 void log(LogLevel level, const std::string message)
 {
@@ -24,22 +35,22 @@ void log(LogLevel level, const std::string message)
 	{
 		switch (level) {
 			case DEBUG:
-				std::cout << BLUE << "[DEBUG]: " << message << RESET << std::endl;
+				std::cout << BLUE << "[" << log_time() << "]" << "[DEBUG]: " << message << RESET << std::endl;
 				break;
 			case INFO:
-				std::cout << WHITE << "[INFO]: " << message << RESET << std::endl;
+				std::cout << WHITE << "[" << log_time() << "]"<< "[INFO]: " << message << RESET << std::endl;
 				break;
 			case CMD:
-				std::cout << CYAN << "[CMD]: " << message << RESET << std::endl;
+				std::cout << CYAN << "[" << log_time() << "]"<< "[CMD]: " << message << RESET << std::endl;
 				break;
 			case REPLY:
-				std::cout << GREEN << "[REPLY]: " << message << RESET << std::endl;
+				std::cout << GREEN << "[" << log_time() << "]"<< "[REPLY]: " << message << RESET << std::endl;
 				break;
 			case WARN:
-				std::cout << YELLOW << "[WARN]: " << message << RESET << std::endl;
+				std::cout << YELLOW << "[" << log_time() << "]"<< "[WARN]: " << message << RESET << std::endl;
 				break;
 			case ERROR:
-				std::cout << RED <<"[ERROR]: " << message << RESET <<std::endl;
+				std::cout << RED << "[" << log_time() << "]"<<"[ERROR]: " << message << RESET <<std::endl;
 				break;
 		}
 		std::cout << std::flush;
