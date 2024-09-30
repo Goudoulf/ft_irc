09cc6fd62a6ad5d3473772ea8b1298ea7acd8529
Camille Assie <cassie@z1r12p5.42lyon.fr>
2024-09-30 17:00:20 +0200

    feat: more

--- includes/ParamBuilder.hpp
+++ includes/ParamBuilder.hpp
@@ -1,18 +1,17 @@
 #pragma once
-#include <iostream>
-#include <vector>
 #include <string>
 #include <functional>
 
-class ParamBuilder {
+class ParamTemplate {
 public:
-    ParamBuilder& param(const std::string& param);
-    ParamBuilder& validate(std::function<bool(const std::string&)> validator);
+
+    ParamTemplate& param(const std::string& param);
+    ParamTemplate& validate(std::function<bool(const std::string&)> validator);
     const std::string& build();
 
 private:
+
     std::string _param;
-    bool _isValid = true; // To track if the param is valid
+    bool _isValid = true;
 };
 
-// Set the parameter in the ParamBuilder
