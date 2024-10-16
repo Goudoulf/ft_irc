81bbdd9dd67e42b23bec8b1d957173f6f46dcf02
Camille Assie <cassie@z3r12p1.42lyon.fr>
2024-10-16 13:00:03 +0200

    feat: add mode pre parsing

--- src/cmds/CommandDirector.cpp
+++ src/cmds/CommandDirector.cpp
@@ -4,10 +4,42 @@
 #include <sstream>
 #include <string>
 #include <map>
+#include <vector>
 
 void	CommandDirector::addCommand(const TemplateBuilder *command)
 {
 	_commandList.insert(std::make_pair(command->getName(), command));
+}
+
+std::vector<std::string> parseMode(std::vector<std::string> parsedParams)
+{
+    std::vector<std::string> parsedMode;
+    
+    std::string temp;
+    for (std::vector<std::string>::iterator it = parsedParams.begin(); it != parsedParams.end(); ++it)
+    {
+	if (it == parsedParams.begin())
+	{
+	    temp = *it;
+	}
+	else if ((*it)[0] == '+' || (*it)[0] == '-')
+	{
+	    parsedMode.push_back(temp);
+	    temp.clear();
+	    temp = *it;
+	}
+	else if ((*it)[0] != '+' && (*it)[0] != '-')
+	    temp += " " + *it;
+	if ((it + 1) == parsedParams.end())
+	    parsedMode.push_back(temp);
+    }
+    std::cout << "MODE PARAM {" << std::endl;
+    for (std::vector<std::string>::iterator it = parsedMode.begin(); it != parsedMode.end(); it++)
+    {
+	 std::cout << *it << std::endl;
+    }
+    std::cout << "}" << std::endl;
+    return parsedMode;
 }
 
 void	CommandDirector::parseCommand(int fd, std::string buffer, IRCServer& server)
@@ -51,6 +83,8 @@ void	CommandDirector::parseCommand(int fd, std::string buffer, IRCServer& server
 	} 
 	parsedParams.push_back(param);
     }
+    if (command == "MODE")
+	parsedParams = parseMode(parsedParams);
     log(INFO, "Director fill param");
     _commandList.find(command)->second->fill_param(fd, parsedParams, server);
 }
