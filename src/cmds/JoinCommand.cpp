90bcfb957860941f3f6637cf3865572fad726f3d
Camille Assie <cassie@z3r12p1.42lyon.fr>
2024-10-26 08:43:50 +0200

    clean

--- src/cmds/JoinCommand.cpp
+++ src/cmds/JoinCommand.cpp
@@ -24,10 +24,6 @@ void	joinChannel2(std::string channel, std::string key, Client *client, IRCServe
 		}
 		chan->add_client(client);
 	}
-	// for (std::map<int, Client*>::iterator it = server->getClients()->begin(); it != server->getClients()->end(); it++) {
-	// 	if (it->second != NULL && chan->inChannel(it->second->getNickname()))
-	// 		message_server("", "JOIN", *client, chan->getChannelName(), it->first);
-	// }
 	chan->sendReply(RPL_JOIN(client->getPrefix(), channel));
 	if (!chan->getTopic().empty())
 		reply_server("332", *client, " " + chan->getChannelName() + " :" + chan->getTopic());
@@ -42,12 +38,11 @@ void JoinCommand::execute(Client *client, const std::map<std::string, std::vecto
         IRCServer *server = IRCServer::getInstance();
 	std::vector<std::string> channels = params.find("channel")->second;
 
-	// Split keys if provided
 	std::vector<std::string> keys;
 	if (params.find("key") != params.end() && !params.find("key")->second.empty()) {
 		keys = params.find("key")->second;
 	}
-	// Process each channel and its corresponding key
+
 	size_t j = 0;
 	for (size_t i = 0; i < channels.size(); i++)
 	{
