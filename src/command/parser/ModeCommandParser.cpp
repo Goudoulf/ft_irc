a7a138a44e494e278dae938a58ba551b530f276e
Camille Assie <cassie@z3r12p1.42lyon.fr>
2024-10-31 17:04:47 +0100

    fix mode

--- src/command/parser/ModeCommandParser.cpp
+++ src/command/parser/ModeCommandParser.cpp
@@ -7,32 +7,31 @@ bool ModeCommandParser::parse(const std::string &input, std::vector<std::vector<
 {
     std::istringstream iss(input);
     std::string token, target, tempString;
-	std::string command;
+    std::string command;
 
-	iss >> command;
-    // if (!(iss >> token))
-	// 	return false;
-	std::vector<std::string> channel;
-	iss >> token;
-	channel.push_back(token);
-	params.push_back(channel);
+    iss >> command;
+    if (!(iss >> token))
+    	return true;
+    std::vector<std::string> channel;
+    channel.push_back(token);
+    params.push_back(channel);
     std::vector<std::string>modes;
-	iss >> token;
-	if (token.at(0) != '+' && token.at(0) != '-')
-		return (false);
-	tempString += token + " ";
-	while (iss >> token)
+    iss >> token;
+    if (token.at(0) != '+' && token.at(0) != '-')
+	return (false);
+    tempString += token + " ";
+    while (iss >> token)
     {
-		if (token.at(0) == '+' || token.at(0) == '-')
-		{
-			modes.push_back(tempString);
-			tempString.erase();
-			tempString += token + " ";
-		}
-		else
-			tempString += token + " ";
+	if (token.at(0) == '+' || token.at(0) == '-')
+	{
+	    modes.push_back(tempString);
+	    tempString.erase();
+	    tempString += token + " ";
+	}
+	else
+	tempString += token + " ";
     }
-	modes.push_back(tempString);
-	params.push_back(modes);
-	return true;
+    modes.push_back(tempString);
+    params.push_back(modes);
+    return true;
 }
