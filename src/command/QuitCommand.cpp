882b235dcbfcff3b71aa85d74a5021dd073e66e2
Camille Assie <cassie@z1r12p5.42lyon.fr>
2024-10-31 15:41:25 +0100

    fix: quit reference issue && multiple command in one buffer and a quit

--- src/command/QuitCommand.cpp
+++ src/command/QuitCommand.cpp
@@ -5,7 +5,7 @@
 #include "cmds.h"
 #include "debug.h"
 
-void	quitServer2(std::string channel, std::string message, Client *client, IRCServer *server)
+void	quitServer2(const std::string &channel, const std::string &message, Client *client, IRCServer *server)
 {
 	Channel *chan = server->findChannel(channel);
 	for (std::map<int, Client*>::iterator it = server->getClients()->begin(); it != server->getClients()->end(); it++)
@@ -20,13 +20,15 @@ void	quitServer2(std::string channel, std::string message, Client *client, IRCSe
 
 void QuitCommand::execute(Client *client, const std::map<std::string, std::vector<std::string> >& params)
 {
-    IRCServer *server = IRCServer::getInstance();
+	(void) params;
+	IRCServer *server = IRCServer::getInstance();
 	for (std::map<std::string, Channel*>::iterator _it = server->getChannels()->begin(); _it != server->getChannels()->end();)
 	{
 		std::map<std::string, Channel *>::iterator _it2 = _it;
 		_it2++;
 		if (_it->second && _it->second->inChannel(client->getNickname()))
-			quitServer2(_it->second->getChannelName() , params.find("message")->second[0], client, server);
+			quitServer2(_it->second->getChannelName() , "", client, server);
+			//quitServer2(_it->second->getChannelName() , params.find("message")->second[0], client, server);
 		_it = _it2;
 	}
 	server->removeClient(client);
